e2d675f755b03fdb6d27bca45a9a8c125f892c2f
blockout22 <kie1k93@gmail.com>
2022-12-03 18:24:16 +0000

    Update custom string to accept variables

--- src/main/java/ovs/graph/node/NodeCustomString.java
+++ src/main/java/ovs/graph/node/NodeCustomString.java
@@ -3,12 +3,16 @@ package ovs.graph.node;
 import imgui.type.ImString;
 import ovs.graph.Graph;
 import ovs.graph.PinData;
+import ovs.graph.pin.Pin;
 import ovs.graph.pin.PinString;
 import ovs.graph.pin.PinVar;
 
 public class NodeCustomString extends Node{
 
     PinString input = new PinString();
+    PinVar pinInput0 = new PinVar();
+    PinVar pinInput1 = new PinVar();
+    PinVar pinInput2 = new PinVar();
     PinVar output = new PinVar();
 
     public NodeCustomString(Graph graph) {
@@ -18,6 +22,18 @@ public class NodeCustomString extends Node{
         input.setNode(this);
         addCustomInput(input);
 
+        pinInput0.setNode(this);
+        pinInput0.setName("{0}");
+        addCustomInput(pinInput0);
+
+        pinInput1.setNode(this);
+        pinInput1.setName("{1}");
+        addCustomInput(pinInput1);
+
+        pinInput2.setNode(this);
+        pinInput2.setName("{2}");
+        addCustomInput(pinInput2);
+
         output.setNode(this);
         addCustomOutput(output);
     }
@@ -25,9 +41,43 @@ public class NodeCustomString extends Node{
     @Override
     public void execute() {
         PinData<ImString> inputData = input.getData();
+        PinData<ImString> input0Data = pinInput0.getData();
+        PinData<ImString> input1Data = pinInput1.getData();
+        PinData<ImString> input2Data = pinInput2.getData();
         PinData<ImString> outputData = output.getData();
 
-        outputData.getValue().set("Custom String(\"" + inputData.getValue().get() + "\")");
+        String toOutput = "Custom String(\"" + inputData.getValue().get() + "\"";
+
+        if(pinInput0.isConnected()){
+            Pin connectedPin = pinInput0.getConnectedPin();
+
+            PinData<ImString> connectedData = connectedPin.getData();
+            input0Data.getValue().set(connectedData.getValue().get());
+
+            toOutput += ", " + input0Data.getValue().get();
+        }
+
+        if(pinInput1.isConnected()){
+            Pin connectedPin = pinInput1.getConnectedPin();
+
+            PinData<ImString> connectedData = connectedPin.getData();
+            input1Data.getValue().set(connectedData.getValue().get());
+
+            toOutput += ", " + input1Data.getValue().get();
+        }
+
+        if(pinInput2.isConnected()){
+            Pin connectedPin = pinInput2.getConnectedPin();
+
+            PinData<ImString> connectedData = connectedPin.getData();
+            input2Data.getValue().set(connectedData.getValue().get());
+
+            toOutput += ", " + input2Data.getValue().get();
+        }
+
+
+
+        outputData.getValue().set(toOutput + ")");
     }
 
     @Override
