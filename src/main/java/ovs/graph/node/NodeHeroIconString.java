9431abbbf0a324e8822e22d935e954dd20d68f79
blockout22 <kie1k93@gmail.com>
2022-12-12 16:19:45 +0000

    Added Node Hero Icon String

---
+++ src/main/java/ovs/graph/node/NodeHeroIconString.java
@@ -0,0 +1,46 @@
+package ovs.graph.node;
+
+import imgui.type.ImString;
+import ovs.graph.Graph;
+import ovs.graph.PinData;
+import ovs.graph.pin.PinVar;
+
+public class NodeHeroIconString extends Node{
+
+    PinVar heroPin = new PinVar();
+
+    PinVar output = new PinVar();
+
+    public NodeHeroIconString(Graph graph) {
+        super(graph);
+        setName("Hero Icon String");
+
+        heroPin.setNode(this);
+        heroPin.setName("Hero");
+        addCustomInput(heroPin);
+
+        output.setNode(this);
+        addCustomOutput(output);
+    }
+
+    @Override
+    public void execute() {
+        PinData<ImString> heroData = heroPin.getData();
+        PinData<ImString> outputData = output.getData();
+
+        handlePinStringConnection(heroPin, heroData, "Hero(Ana)");
+
+        outputData.getValue().set("Hero Icon String(" + heroData.getValue().get() + ")");
+    }
+
+    @Override
+    public String getOutput() {
+        PinData<ImString> outputData = output.getData();
+        return outputData.getValue().get();
+    }
+
+    @Override
+    public void UI() {
+
+    }
+}
