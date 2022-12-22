b7e70b30d54c0d3a8f9bd6c4039a840448cc47b5
blockout22 <kie1k93@gmail.com>
2022-12-22 16:15:36 +0000

    Added Node Start Game Mode

---
+++ src/main/java/ovs/graph/node/NodeStartGameMode.java
@@ -0,0 +1,34 @@
+package ovs.graph.node;
+
+import imgui.type.ImString;
+import ovs.graph.Graph;
+import ovs.graph.PinData;
+import ovs.graph.pin.PinAction;
+
+public class NodeStartGameMode extends Node{
+
+    PinAction output = new PinAction();
+
+    public NodeStartGameMode(Graph graph) {
+        super(graph);
+        setName("Start Game Mode");
+    }
+
+    @Override
+    public void execute() {
+        PinData<ImString> outputData = output.getData();
+
+        outputData.getValue().set("Start Game Mode;");
+    }
+
+    @Override
+    public String getOutput() {
+        PinData<ImString> outputData = output.getData();
+        return outputData.getValue().get();
+    }
+
+    @Override
+    public void UI() {
+
+    }
+}
