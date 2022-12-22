7625f5cb7db90916e6f5adfe644769fc8cc94ad6
blockout22 <kie1k93@gmail.com>
2022-12-22 09:50:04 +0000

    Added Nodes for Abilities

---
+++ src/main/java/ovs/graph/node/NodeIsUsingAbility1.java
@@ -0,0 +1,46 @@
+package ovs.graph.node;
+
+import imgui.type.ImString;
+import ovs.graph.Graph;
+import ovs.graph.PinData;
+import ovs.graph.pin.PinVar;
+
+public class NodeIsUsingAbility1 extends Node{
+
+    PinVar pinPlayer = new PinVar();
+
+    PinVar output = new PinVar();
+
+    public NodeIsUsingAbility1(Graph graph) {
+        super(graph);
+        setName("Is Using Ability 1");
+
+        pinPlayer.setNode(this);
+        pinPlayer.setName("Player");
+        addCustomInput(pinPlayer);
+
+        output.setNode(this);
+        addCustomOutput(output);
+    }
+
+    @Override
+    public void execute() {
+        PinData<ImString> playerData = pinPlayer.getData();
+        PinData<ImString> outputData = output.getData();
+
+        handlePinStringConnection(pinPlayer, playerData);
+
+        outputData.getValue().set("Is Using Ability 1(" + playerData.getValue().get() + ")");
+    }
+
+    @Override
+    public String getOutput() {
+        PinData<ImString> outputData = output.getData();
+        return outputData.getValue().get();
+    }
+
+    @Override
+    public void UI() {
+
+    }
+}
---
+++ src/main/java/ovs/graph/node/NodeSetAbility1Enabled.java
@@ -0,0 +1,55 @@
+package ovs.graph.node;
+
+import imgui.type.ImString;
+import ovs.graph.Graph;
+import ovs.graph.PinData;
+import ovs.graph.pin.PinAction;
+import ovs.graph.pin.PinVar;
+
+public class NodeSetAbility1Enabled extends Node{
+
+    PinVar pinPlayer = new PinVar();
+    PinVar pinBool = new PinVar();
+
+    PinAction output = new PinAction();
+
+    public NodeSetAbility1Enabled(Graph graph) {
+        super(graph);
+        setName("Set Ability 1 Enabled");
+
+        pinPlayer.setNode(this);
+        pinPlayer.setName("Player");
+        addCustomInput(pinPlayer);
+
+        pinBool.setNode(this);
+        pinBool.setName("True/False");
+        addCustomInput(pinBool);
+
+        output.setNode(this);
+        addCustomOutput(output);
+    }
+
+    @Override
+    public void execute() {
+        PinData<ImString> playerData = pinPlayer.getData();
+        PinData<ImString> boolData = pinBool.getData();
+        PinData<ImString> outputData = output.getData();
+
+        handlePinStringConnection(pinPlayer, playerData);
+        handlePinStringConnection(pinBool, boolData, "True");
+
+        outputData.getValue().set("Set Ability 1 Enabled(" + playerData.getValue().get() + ", " + boolData.getValue().get() + ");");
+
+    }
+
+    @Override
+    public String getOutput() {
+        PinData<ImString> outputData = output.getData();
+        return outputData.getValue().get();
+    }
+
+    @Override
+    public void UI() {
+
+    }
+}
