a45b4506d71798b7e6d0bb78fca5fb43fda1a413
blockout22 <kie1k93@gmail.com>
2022-12-22 16:18:06 +0000

    Added Node Stop Camera

---
+++ src/main/java/ovs/graph/node/NodeStopCamera.java
@@ -0,0 +1,47 @@
+package ovs.graph.node;
+
+import imgui.type.ImString;
+import ovs.graph.Graph;
+import ovs.graph.PinData;
+import ovs.graph.pin.PinAction;
+import ovs.graph.pin.PinVar;
+
+public class NodeStopCamera extends Node{
+
+    PinVar pinPlayer = new PinVar();
+    PinAction output = new PinAction();
+
+    public NodeStopCamera(Graph graph) {
+        super(graph);
+        setName("Stop Camera");
+
+        pinPlayer.setNode(this);
+        pinPlayer.setName("Player");
+        addCustomInput(pinPlayer);
+
+        output.setNode(this);
+        addCustomOutput(output);
+    }
+
+    @Override
+    public void execute() {
+        PinData<ImString> playerData = pinPlayer.getData();
+        PinData<ImString> outputData = output.getData();
+
+        handlePinStringConnection(pinPlayer, playerData);
+
+        outputData.getValue().set("Stop Camera(" + playerData.getValue().get() + ");");
+
+    }
+
+    @Override
+    public String getOutput() {
+        PinData<ImString> outputData = output.getData();
+        return outputData.getValue().get();
+    }
+
+    @Override
+    public void UI() {
+
+    }
+}
