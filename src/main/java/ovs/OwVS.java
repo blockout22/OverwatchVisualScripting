88c70cd9a801bf8c85b05588e8233253e50ab759
blockout22 <kie1k93@gmail.com>
2023-09-24 16:00:51 +0100

    Relocation of String data

--- src/main/java/ovs/OwVS.java
+++ src/main/java/ovs/OwVS.java
@@ -156,9 +156,8 @@ public class OwVS {
             }catch (Exception e){}
         }
 
-        File file = new File("src\\main\\resources\\heroes.txt");
-        System.out.println(file.getAbsoluteFile());
-//        Global.writeArrayToFile(Global.heroes, file);
+        File file = new File("src\\main\\resources\\stats.txt");
+        Global.writeArrayToFile(Global.stats, file);
         new OwVS();
     }
 }
